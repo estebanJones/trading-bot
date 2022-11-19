7f6eddb73920272cd5b13b824c2d27ba7e58bdc5
estebanJones <teixeirac.jordan@gmail.com>
2022-11-19 19:56:22 +0100

    logging + purge + facto

--- strategies/grid_spot/strategy-binance.py
+++ strategies/grid_spot/strategy-binance.py
@@ -3,81 +3,89 @@ from binance.spot import Spot
 import pandas as pd
 from datetime import datetime
 import json
+import logging
 
-environment = "test"
+logging.basicConfig(level=logging.NOTSET)
+context = "strategy-binance.py"
+logger = logging.getLogger(context)
+
+IS_TESTNET = False
+global_environment = "prod"
+
+global_pair = "BNBETH"
+global_symbol = "BNB/ETH"
+global_coin1 = "BNB"
+global_coin2 = "ETH"
+PATH_KEYS = "/home/esteban/python-workspace/trading/binance_bot/secret.json"
+PATH_OPEN_ORDERS = "/home/esteban/python-workspace/trading/binance_bot/strategies/grid_spot/last_data.json"
 
 def get_api_keys() -> str:
-    f = open("/home/esteban/Documents/trading/binance_bot/secret.json")
+    f = open(PATH_KEYS)
     secret = json.load(f)
     f.close()
     return secret
 
 def get_last_data() -> str:
-    f = open("/home/esteban/Documents/trading/binance_bot/strategies/grid_spot/last_data.json")
+    f = open(PATH_OPEN_ORDERS)
     last_data = json.load(f)
     f.close()
     return last_data
 
 def new_order(c: Spot, side: enumerate, q: float, p: float):
-    # response_order = new_order_testnet(client, Client.SIDE_BUY, 1.0, 4000.0)
-    print("client {}".format(c))
-    response = c.new_order(
-        symbol='BNBETH',
-        side=side,
-        type=Client.ORDER_TYPE_LIMIT_MAKER,
-        quantity=q,
-        price=p
-    )
-    return response
-    
-def get_exchange_info():
-    exchangeInfo = client.exchange_info("BNBETH")
-    print("exchangeInfo: {}".format(exchangeInfo))
+    if(IS_TESTNET):
+        return c.new_order_test(
+            symbol=global_pair,
+            side=side,
+            type=Client.ORDER_TYPE_LIMIT_MAKER,
+            quantity=q,
+            price=p
+        )
+    else:
+        return c.new_order(
+            symbol=global_pair,
+            side=side,
+            type=Client.ORDER_TYPE_LIMIT_MAKER,
+            quantity=q,
+            price=p
+        )
 
-def new_order_testnet(c: Spot, side: enumerate, q: float, p: float):
-    # response_order = new_order_testnet(client, Client.SIDE_BUY, 1.0, 4000.0)
-    print("client {}".format(c))
-    response = c.new_order_test(
-        symbol='BNBETH',
-        side=side,
-        type=Client.ORDER_TYPE_LIMIT_MAKER,
-        quantity=q,
-        price=p
-    )
-    return response
+def get_realtime_price(pair: str):
+    client = Client(apiKeys[global_environment]['public'], apiKeys[global_environment]['private'], testnet=IS_TESTNET)
+    last_trade = client.get_recent_trades(symbol=pair, limit=1)
+    return last_trade[0]["price"]
+
+# Not Already used
+def get_exchange_info():
+    exchangeInfo = client.exchange_info(global_pair)
+    logger.info("BINANCE INFOS:\n {}".format(exchangeInfo))
 
 def get_account_infos():
-    client = Client(apiKeys[environment]['public'], apiKeys[environment]['private'], testnet=True)
+    client = Client(apiKeys[global_environment]['public'], apiKeys[global_environment]['private'], testnet=IS_TESTNET)
     accountInfos = client.get_account()
     return accountInfos
 
-def get_balance_usdt_btc():
-    usdtBalance = ""
-    btcBalance = ""
+def get_balance_eth_btc():
+    bnbBalance = ""
+    ethBalance = ""
 
     balances = get_account_infos()['balances']
     for balance in balances:
-        if balance["asset"] == "BNB":
-            usdtBalance = balance["free"]
-        elif balance["asset"] == "ETH":
-            btcBalance = balance["free"]
+        if balance["asset"] == global_coin1:
+            bnbBalance = balance["free"]
+        elif balance["asset"] == global_coin2:
+            ethBalance = balance["free"]
 
-    return {"ETH": usdtBalance, "BNB": btcBalance}
+    return {global_coin1: bnbBalance, global_coin2: ethBalance}
+
 
 apiKeys = get_api_keys()
 last_data = get_last_data()
 
-def get_realtime_price_testnet(pair: str):
-    client = Client(apiKeys[environment]['public'], apiKeys[environment]['private'], testnet=True)
-    last_trade = client.get_recent_trades(symbol=pair, limit=1)
-    return last_trade[0]["price"]
-
-print("Binance Bot launch !")
 
 def custom_grid(
     first_price, last_order_down = 0.02, last_order_up = 0.02, down_grid_len=10, up_grid_len=20
 ):
-    print("Price {}".format(first_price))
+    logger.info("Starting price to analyze {}".format(first_price))
     down_pct_unity = last_order_down / down_grid_len
     up_pct_unity = last_order_up / up_grid_len
 
@@ -94,101 +102,108 @@ def custom_grid(
 
 
 now = datetime.now()
-print(now.strftime("%d-%m %H:%M:%S"))
 
-symbol = "BNB/ETH"
-coin1 = "BNB"
-coin2 = "ETH"
+logger.debug("Binance Bot started !")
+logger.info("Today is the {}".format(now.strftime("%d-%m %H:%M:%S")))
+
+
 total_orders = 10
+currentPrice = get_realtime_price(global_pair)
 
-currentPrice = get_realtime_price_testnet("BNBETH")
-print("currentPrice: {}".format(currentPrice))
+logger.info("Price: 1 BNB = {} ETH".format(currentPrice))
 
-client = Spot(apiKeys[environment]['public'], apiKeys[environment]['private'], base_url='https://testnet.binance.vision')
+# , base_url='https://testnet.binance.vision'
+client = Spot(apiKeys[global_environment]['public'], apiKeys[global_environment]['private'])
 orders_list = []
-print("orders_list avant: {}".format(orders_list))
-# response_order = new_order_testnet(client, Client.SIDE_BUY, 1, 4000.0000)
 
-for order in client.get_open_orders("BNBETH"):
+for order in client.get_open_orders(global_pair):
     orders_list.append(order)
+
+logger.info("Existing open orders:\n {}".format(orders_list))
 
 df_order = pd.DataFrame(orders_list)
 if df_order.empty == False:
     df_order["price"] = pd.to_numeric(df_order["price"])
     df_order["origQty"] = pd.to_numeric(df_order["origQty"])
-# print(df_order)
 
-print("{}".format(get_account_infos()))
-balances = get_balance_usdt_btc()
+balances = get_balance_eth_btc()
 
-coin1_balance = float(balances[coin1])
-coin2_balance = float(balances[coin2])
-print("coin1_balance: {}".format(coin1_balance))
-print("coin2_balance: {}".format(coin2_balance))
+coin1_balance = float(balances[global_coin1])
+coin2_balance = float(balances[global_coin2])
+logger.info("BNB balance: {}".format(coin1_balance))
+logger.info("ETH  balance: {}".format(coin2_balance))
 
 if (
     df_order.empty
     or len(df_order.loc[df_order["side"] == "BUY"]) == 0
     or len(df_order.loc[df_order["side"] == "SELL"]) == 0
 ):
-    print("create new grid")
+    logger.info("Creating new grid...")
     grid_buy, grid_sell = custom_grid(
-        int(float(currentPrice)),
+        float(currentPrice),
         last_order_down = 0.02, last_order_up = 0.02, down_grid_len=10, up_grid_len=20
     )
+    logger.debug("grid_buy={}".format(grid_buy))
+    logger.debug("grid_sell={}".format(grid_sell))
+    logger.debug("float(currentPrice)={}".format(float(currentPrice)))
+    for price_buy in grid_buy:
+        logger.debug("coin2_balance / price_buy)={} - len(grid_buy)={}".format(coin2_balance / price_buy, len(grid_buy)))
+        quantity_buy = round((coin2_balance / price_buy) / len(grid_buy), 8)
+        # new_order(
+        #     client, Client.SIDE_BUY, quantity_buy, price_buy
+        # )
+        logger.info("New orders buy done: Quantity={} - Price={}".format(quantity_buy, price_buy))
 
-    for buy in grid_buy:
-        quantity = round((coin2_balance / buy) / len(grid_buy), 8)
-        print("buy: {}".format(buy))
-        print("quantite rounded {}".format(quantity))
-        get_exchange_info()
-        new_order_testnet(
-            client, Client.SIDE_BUY, quantity, buy
-        )
-
-    for sell in grid_sell:
-        # print(sell,coin1_balance/len(grid_sell))
-        quantity = round(coin1_balance / len(grid_sell), 8)
-        print("sell: {}".format(sell))
-        print("quantite rounded {}".format(quantity))
-        new_order_testnet(
-            client, Client.SIDE_SELL, quantity, sell
-        )
+    for price_sell in grid_sell:
+        quantity_sell = round(coin1_balance / len(grid_sell), 8)
+        # new_order(
+        #     client, Client.SIDE_SELL, quantity_sell, price_sell
+        # )
+        logger.info("New orders sell done: Quantity={} - Price={}".format(quantity_sell, price_sell))
 
 elif total_orders == len(df_order):
-    print("no new orders")
+    logger.info("Existing orders are always active. New orders: None")
 else:
+    logger.info("Grid not full. Calculating grid completion...")
     buy_order_to_create = last_data["number_of_sell_orders"] - len(
         df_order.loc[df_order["side"] == "SELL"]
     )
     sell_order_to_create = last_data["number_of_buy_orders"] - len(
         df_order.loc[df_order["side"] == "BUY"]
     )
-    print("Create", buy_order_to_create, "new buy orders")
-    print("Create", sell_order_to_create, "new sell orders")
-    last_buy_order = df_order.loc[df_order["side"] == "BUY"]["price"].max()
-    last_sell_order = df_order.loc[df_order["side"] == "SELL"]["price"].min()
+    logger.info("Total new orders buy {}".format(buy_order_to_create))
+    logger.info("Total new orders sell {}".format(sell_order_to_create))
 
+    last_buy_order = df_order.loc[df_order["side"] == "BUY"]["price"].max()
+    logger.debug("Last max buy: {}".format(last_buy_order))
+    last_sell_order = df_order.loc[df_order["side"] == "SELL"]["price"].min()
+    logger.debug("Last min sell: {}".format(last_sell_order))
     diff_buy = (currentPrice - last_buy_order) / (buy_order_to_create + 1)
+    logger.debug("diff_buy: {}".format(diff_buy))
+    logger.debug("currentPrice: {}".format(currentPrice))
+    logger.debug("last_buy_order: {}".format(last_buy_order))
+    logger.debug("buy_order_to_create: {}".format(buy_order_to_create))
     diff_sell = (last_sell_order - currentPrice) / (sell_order_to_create + 1)
+    logger.debug("diff_sell: {}".format(diff_sell))
+    logger.debug("currentPrice: {}".format(currentPrice))
+    logger.debug("last_sell_order: {}".format(last_sell_order))
+    logger.debug("sell_order_to_create: {}".format(sell_order_to_create))
 
     for i in range(buy_order_to_create):
-        # print("buy",currentPrice - diff_buy*(i+1))
         quantity = round((coin2_balance / currentPrice) / buy_order_to_create,  8)
         buy = currentPrice - diff_buy * (i + 1)
-        new_order_testnet(
-            client, Client.SIDE_BUY, quantity, buy
-        )
+        # new_orde(
+        #     client, Client.SIDE_BUY, quantity, buy
+        # )
     for i in range(sell_order_to_create):
-        # print("sell",currentPrice + diff_sell*(i+1))
         quantity = round(coin1_balance / sell_order_to_create,  8)
         sell = currentPrice + diff_sell * (i + 1)
-        new_order_testnet(
-            client, Client.SIDE_SELL, quantity, sell
-        )
+        # new_order(
+        #     client, Client.SIDE_SELL, quantity, sell
+        # )
 
 orders_list = []
-for order in client.get_open_orders("BNBETH"):
+for order in client.get_open_orders(global_pair):
     orders_list.append(order)
 
 df_order = pd.DataFrame(orders_list)
@@ -199,5 +214,5 @@ else:
     last_data["number_of_buy_orders"] = 0
     last_data["number_of_sell_orders"] = 0
 
-with open("./live_tools/strategies/grid_spot/last_data.json", "w") as outfile:
+with open(PATH_OPEN_ORDERS, "w") as outfile:
     json.dump(last_data, outfile)
